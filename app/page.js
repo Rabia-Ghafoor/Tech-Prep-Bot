17eeef510e771efd6e27020708d47459129f0c6e
Rabia Ghafoor <57128418+Rabia-Ghafoor@users.noreply.github.com>
2024-08-08 00:37:19 -0400

    fixed code

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 'use client'
 
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, TextField, Button } from "@mui/material";
 import { useState } from "react";
+import Image from 'next/image';
 
 
 export default function Home() {
@@ -11,7 +12,51 @@ export default function Home() {
     content: `Hi, I am the Customer Support Chat Bot. How can I assist you today?`,
   }]);
 
-  const [newMessage, setNewMessage] = useState('');
+  const [message, setMessage] = useState('');
+
+  // helper function
+  const sendMessage = async () => {
+    setMessage('');
+    setMessages((messages) => [
+      ...messages,
+      { role: "user", content: message },
+      { role: "assistant", content: "" },
+    ]);
+
+    const response = await fetch('api/chat', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify([...messages, { role: "user", content: message }]),
+    });
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+
+    let result = '';
+    reader.read().then(function processText({ done, value }) {
+      if (done) {
+        return result;
+      }
+
+      const text = decoder.decode(value || new Int8Array(), { stream: true });
+      setMessages((messages) => {
+        let lastMessage = messages[messages.length - 1];
+        let otherMessages = messages.slice(0, messages.length - 1);
+
+        return ([
+          ...otherMessages,
+          {
+            ...lastMessage,
+            content: lastMessage.content + text,
+          },
+        ]);
+      });
+
+      return reader.read().then(processText);
+    });
+  };
 
   return (
     <Box 
@@ -61,6 +106,18 @@ export default function Home() {
               </Box>
             </Box>
           ))}
+        </Stack>
+
+        <Stack direction="row" spacing={2}>
+          <TextField 
+            label="message" 
+            fullWidth
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant="contained" onClick={sendMessage}> 
+            Send
+          </Button>
         </Stack>
       </Stack>
     </Box>
